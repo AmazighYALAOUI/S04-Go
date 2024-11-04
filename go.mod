99c1aa5e116e95a80b1cad2d688b1e3bf816c7ba
AmazighYALAOUI <amazigh.yalaoui@oclock.school>
2024-11-04 10:57:34 +0000

    Fonction et fstring en Go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/AmazighYALAOUI/S04-Go.git
+
+go 1.22.7
--- main.go
+++ main.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+func add(a int, b int) int {
+	return a + b
+}
+
 func main() {
 	fmt.Println("Hello World !")
+	//var somme int = add(14, 27) ou bien:
+	somme := add(14, 77)
+	fmt.Println(somme)
+	//f string
+	fmt.Printf("La somme est de : %v\n", somme)
 }
